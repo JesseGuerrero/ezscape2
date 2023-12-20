333f1140c9b67f036222189ccd5b3c74c91af5a3
Trenton Kress <trenton.kress@gmail.com>
2023-12-20 00:54:35 -0800

    No tutorial island

--- src/main/java/com/rs/anarchy/LoginModifiers.kt
+++ src/main/java/com/rs/anarchy/LoginModifiers.kt
@@ -1,7 +1,13 @@
 package com.rs.anarchy
 
+import com.rs.Settings
 import com.rs.engine.command.Commands
+import com.rs.game.World
+import com.rs.game.content.achievements.Achievement
+import com.rs.game.content.tutorialisland.TutorialIslandController
+import com.rs.game.model.entity.player.managers.InterfaceManager
 import com.rs.lib.game.Rights
+import com.rs.lib.game.Tile
 import com.rs.plugin.annotations.ServerStartupEvent
 import com.rs.plugin.kts.onLogin
 
@@ -10,6 +16,18 @@ fun mapLoginModifiers() {
     onLogin {
         it.player.apply {
             checkZone(this, this.chunkId, true)
+            if (controllerManager.isIn(TutorialIslandController::class.java)) {
+                setIronMan(false)
+                isChosenAccountType = true
+                World.sendWorldMessage("<img=5><col=FF0000>" + displayName + " has just joined " + Settings.getConfig().serverName + "!</col>", false)
+                tele(Tile.of(Settings.getConfig().playerStartTile))
+                controllerManager.forceStop()
+                interfaceManager.flashTabOff()
+                interfaceManager.sendSubDefaults(*InterfaceManager.Sub.ALL_GAME_TABS)
+                giveStarter()
+                interfaceManager.sendAchievementComplete(Achievement.THE_JOURNEY_BEGINS_3521)
+                appearance.generateAppearanceData()
+            }
         }
     }
 
