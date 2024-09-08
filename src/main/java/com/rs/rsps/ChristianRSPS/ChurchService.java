09b2eddfa6e30e06cdc06fa35e2beea0d4b40f7b
Jesse Guerrero <jesus.guerrero6@my.utsa.edu>
2024-09-08 18:47:30 -0500

    Back to EZScape

---
+++ src/main/java/com/rs/rsps/ChristianRSPS/ChurchService.java
@@ -0,0 +1,247 @@
+package com.rs.rsps.ChristianRSPS;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.rs.engine.cutscene.Cutscene;
+import com.rs.engine.pathfinder.Direction;
+import com.rs.game.content.skills.magic.TeleType;
+import com.rs.game.map.instance.Instance;
+import com.rs.game.model.entity.Entity;
+import com.rs.game.model.entity.npc.NPC;
+import com.rs.game.model.entity.player.Controller;
+import com.rs.game.model.entity.player.Player;
+import com.rs.game.model.object.GameObject;
+import com.rs.game.tasks.Task;
+import com.rs.game.tasks.WorldTasks;
+import com.rs.lib.game.Tile;
+import com.rs.lib.util.Utils;
+import com.rs.plugin.annotations.PluginEventHandler;
+import com.rs.plugin.annotations.ServerStartupEvent;
+import com.rs.plugin.handlers.PlayerStepHandler;
+import com.rs.utils.Ticks;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@PluginEventHandler
+public class ChurchService extends Controller {
+    protected static boolean serviceStarted = false;
+    protected static String[][] sermons = null;
+    private static String[] praises = {
+            "Hallelujah.",
+            "Thank you Jesus.",
+            "Have mercy on us.",
+            "We praise you Lord.",
+            "Praise God.",
+            "In Jesus name.",
+            "I will dance like David.",
+            "Amen",
+            "Glory to God.",
+            "Bless the Lord, O my soul.",
+            "Holy, holy, holy.",
+            "Jesus is Lord.",
+            "We exalt your name.",
+            "Your grace is sufficient.",
+            "The Lord is my shepherd.",
+            "He is worthy of all praise.",
+            "The Lord reigns forever.",
+            "Blessed be your name.",
+            "Great is your faithfulness.",
+            "Thank you for your mercy.",
+            "The Lord is my rock and salvation.",
+            "Your love endures forever.",
+            "We lift your name on high.",
+            "You are the King of kings.",
+            "All glory to your name."
+    };
+    public static Instance church = null;
+    private static Tile entranceTile = null;
+    protected static Tile serviceTile = null;
+    private static final Tile OUTSIDE = Tile.of(3237, 3209, 0);
+
+    protected static NPC priest;
+    protected static List<NPC> npcs = new ArrayList<>();
+
+    public ChurchService() {
+        super();
+    }
+
+    @Override
+    public void start() {
+        player.lock();
+        Tile start = entranceTile;
+        player.playCutscene(new Cutscene() {
+            @Override
+            public void construct(Player player) {
+                player.lock();
+                setEndTile(start);
+                fadeIn(0);
+                hideMinimap();
+                delay(3);
+                playerMove(start.x(), start.y(), start.getPlane(), Entity.MoveType.TELE, 0);
+                delay(3);
+                fadeOut(0);
+                player.unlock();
+            }
+        });
+    }
+
+    @Override
+    public boolean sendDeath() {
+        player.stopAll();
+        player.reset();
+        player.tele(OUTSIDE);
+        forceClose();
+        return false;
+    }
+
+    @Override
+    public boolean login() {
+        player.tele(OUTSIDE);
+        forceClose();
+        return false;
+    }
+
+    @Override
+    public boolean logout() {
+        player.unlock();
+        return false;
+    }
+
+    @Override
+    public void forceClose() {
+        player.getPackets().setBlockMinimapState(0);
+        player.unlock();
+        removeController();
+    }
+
+    @Override
+    public void moved() {
+        Tile tile = player.getTile();
+        if(tile.matches(entranceTile) || tile.matches(Tile.of(entranceTile.x(), entranceTile.y() + 1, entranceTile.plane()))) {
+            player.playCutscene(new Cutscene() {
+                @Override
+                public void construct(Player player) {
+                    player.lock();
+                    setEndTile(OUTSIDE);
+                    fadeIn(0);
+                    hideMinimap();
+                    delay(3);
+                    playerMove(OUTSIDE.x(), OUTSIDE.y(), OUTSIDE.getPlane(), Entity.MoveType.TELE, 0);
+                    delay(3);
+                    fadeOut(0);
+                    player.unlock();
+                    player.getControllerManager().removeControllerWithoutCheck();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onTeleported(TeleType type) {
+        forceClose();
+    }
+
+    public static PlayerStepHandler enterChurch = new PlayerStepHandler(new Tile[] { Tile.of(3238, 3209, 0), Tile.of(3238, 3210, 0)}, e -> {
+        if(e.getPlayer().getI("ChurchType", 0) > 0 || true) { // 0 no church, 1 pentacostal, 2 protestant
+            e.getPlayer().getControllerManager().startController(new ChurchService());
+        }
+    });
+
+
+    public boolean processObjectClick1(GameObject object) {
+        if(object.getId() == 36972 && serviceStarted == false /*&& !player.getDailyB("HasDoneSermon")*/) {
+            serviceStarted = true;
+            WorldTasks.delay(Ticks.fromMinutes(2), () -> {
+                serviceStarted = false;
+            });
+            player.playCutscene(new Sermon());
+            return false;
+        }
+        return true;
+    }
+
+
+    @ServerStartupEvent
+    public static void init() {
+        sermons = loadAllSermons("src/main/java/com/rs/rsps/ChristianRSPS/sermons");
+        spawnNPC(2899, Tile.of(3237, 3208, 0), "Father John", Direction.NORTH, false);
+        ChurchService.church = Instance.of(OUTSIDE, 6, 6, false);
+        church.copyMapAllPlanes(403, 399);
+        WorldTasks.delay(4, () -> {
+            ChurchService.entranceTile = church.getLocalTile(14, 17);
+            ChurchService.serviceTile = church.getLocalTile(21, 15);
+            priest = spawnNPC(456, church.getLocalTile(20, 13), "Father Aereck", Direction.NORTH, false);
+            npcs.add(spawnNPC(0, church.getLocalTile(23, 15), "Hans", Direction.SOUTHWEST, false));
+            npcs.add(spawnNPC(668, church.getLocalTile(17, 17), "Johnathan", Direction.SOUTH, false));
+            npcs.add(spawnNPC(7969, church.getLocalTile(21, 19), "Jack", Direction.SOUTH, false));
+            npcs.add(spawnNPC(8864, church.getLocalTile(18, 19), "Hank", Direction.SOUTH, false));
+            npcs.add(spawnNPC(758, church.getLocalTile(21, 17), "Fred", Direction.SOUTH, false));
+            npcs.add(spawnNPC(4589, church.getLocalTile(17, 15), "Mike", Direction.SOUTHEAST, false));
+        });
+        WorldTasks.scheduleTimer(6, Ticks.fromSeconds(10), (ticks) -> {
+            if(!serviceStarted)
+                npcs.get(Utils.random(npcs.size())).forceTalk(praises[Utils.random(praises.length)]);
+            return true;
+        });
+
+    }
+
+    public static NPC spawnNPC(int id, Tile tile, String name, Direction direction, boolean randomWalk) {
+        NPC npc = new NPC(id, tile);
+        if (name != null)
+            npc.setPermName(name);
+        if (direction != null)
+            npc.setFaceAngle(direction.getAngle());
+        npc.setRandomWalk(randomWalk);
+        return npc;
+    }
+
+    public static String[][] loadAllSermons(String directoryPath) {
+        File directory = new File(directoryPath);
+        List<String[]> sermonsList = new ArrayList<>();
+
+        // Check if the directory exists and is a directory
+        if (directory.exists() && directory.isDirectory()) {
+            // Get all files in the directory
+            File[] files = directory.listFiles((dir, name) -> name.endsWith(".json"));
+
+            if (files != null) {
+                Gson gson = new Gson();
+
+                for (File file : files) {
+                    try (FileReader reader = new FileReader(file)) {
+                        // Parse each file's JSON content
+                        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+
+                        // Extract the "Sermon" array from JSON
+                        JsonArray sermonArray = jsonObject.getAsJsonArray("Sermon");
+
+                        if (sermonArray != null) {
+                            String[] sermonParts = new String[sermonArray.size()];
+
+                            // Convert JsonArray to String array
+                            for (int i = 0; i < sermonArray.size(); i++) {
+                                sermonParts[i] = sermonArray.get(i).getAsString();
+                            }
+
+                            // Add this sermon to the list
+                            sermonsList.add(sermonParts);
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        // Convert the list to a 2D array
+        return sermonsList.toArray(new String[0][]);
+    }
+
+}
---
+++ src/main/java/com/rs/rsps/EZScape.java
@@ -0,0 +1,223 @@
+package com.rs.rsps;
+
+import com.rs.engine.quest.Quest;
+import com.rs.game.content.minigames.treasuretrails.TreasureTrailsManager;
+import com.rs.game.content.quests.shieldofarrav.ShieldOfArrav;
+import com.rs.game.content.skills.fishing.Fish;
+import com.rs.game.content.skills.fishing.FishingSpot;
+import com.rs.game.content.skills.mining.Ore;
+import com.rs.game.content.skills.slayer.Task;
+import com.rs.game.content.skills.thieving.PickPocketableNPC;
+import com.rs.game.model.entity.player.Player;
+import com.rs.lib.game.Item;
+import com.rs.lib.util.Utils;
+import com.rs.plugin.annotations.PluginEventHandler;
+import com.rs.plugin.handlers.LoginHandler;
+import com.rs.utils.DropSets;
+import com.rs.utils.drop.DropTable;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static com.rs.game.content.skills.runecrafting.RunecraftingAltar.WICKED_HOOD_INTER;
+
+@PluginEventHandler
+public class EZScape {
+    public static LoginHandler onLogin = new LoginHandler(e -> {
+        for (Quest quest : Quest.values())
+            if (quest.isImplemented())
+                if (!e.getPlayer().getQuestManager().isComplete(quest)) {
+                    e.getPlayer().getQuestManager().setStage(quest, quest.getHandler().getCompletedStage());
+                    e.getPlayer().getQuestManager().sendQuestStage(quest);
+                    e.getPlayer().getQuestManager().sendQuestPoints();
+                }
+        ShieldOfArrav.setGang(e.getPlayer(), "Phoenix");
+    });
+
+    public static boolean removeIronMan() {
+        return true;
+    }
+
+    public static Item[] tripleNPCDrops(Player killer, int id) {
+        Item[] drop1 = DropTable.calculateDrops(killer, DropSets.getDropSet(id));
+        Item[] drop2 = DropTable.calculateDrops(killer, DropSets.getDropSet(id));
+        Item[] drop3 = DropTable.calculateDrops(killer, DropSets.getDropSet(id));
+        return Stream.of(drop1, drop2, drop3)
+                .flatMap(Arrays::stream)
+                .toArray(Item[]::new);
+    }
+
+    public static void giveTwoExtraOre(Player player, Ore ore) {
+        ore.giveOre(player);
+        ore.giveOre(player);
+    }
+
+    public static void giveTwoExtraFish(Player player, FishingSpot spot, Fish f) {
+        f.giveFish(player, spot);
+        f.giveFish(player, spot);
+    }
+
+    public static boolean giveTwoExtraLog() {
+        return true;
+    }
+
+    public static boolean updateLoyalty(Player player) {
+        player.loyaltyPoints += 800;
+        player.setPestPoints(player.getPestPoints() + 5);
+        if(Utils.randomInclusive(0, 1) == 0)
+            player.soulWarsZeal+=1;
+        if(Utils.randomInclusive(0, 2) == 0)
+            player.scPoints += 1;
+        player.incDailyI("loyaltyTicks");
+        if(player.getDailyI("loyaltyTicks") % 12 == 0) {
+            player.sendMessage("<col=FF0000>You have " + Utils.formatNumber(player.loyaltyPoints) + " loyalty points from play time...");
+            player.sendMessage("<col=FF0000>You have " + Utils.formatNumber(player.getPestPoints()) + " pest control points from play time...");
+            player.sendMessage("<col=FF0000>You have " + Utils.formatNumber(player.soulWarsZeal) + " soul wars zeal from play time...");
+        }
+        return false;
+    }
+
+    public static boolean limit99PestControl(Player player,int skillId) {
+        return player.getSkills().getLevelForXp(skillId) >= 99;
+    }
+
+    public static boolean limitZealUse(Player player, int skill) {
+        boolean is99 = player.getSkills().getLevel(skill) >= 99;
+        if(is99) {
+            player.sendMessage("You can't use zeal on a 99 skill.");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean removeLoyaltyAuraCooldowns() {
+        return true;
+    }
+
+    public static Item[] tenTimesPickPocket(Player player, PickPocketableNPC npcData) {
+        Item[] drop1 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop2 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop3 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop4 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop5 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop6 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop7 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop8 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop9 = DropTable.calculateDrops(player, npcData.getLoot());
+        Item[] drop10 = DropTable.calculateDrops(player, npcData.getLoot());
+        return Stream.of(drop1, drop2, drop3, drop4, drop5, drop6, drop7, drop8, drop9, drop10)
+                .flatMap(Arrays::stream)
+                .toArray(Item[]::new);
+    }
+
+    public static int getFightCavesWave(Player player) {
+        return player.getI("fightCavesWaveI", 1);
+    }
+
+    public static void saveFightCaveWave(Player player, int wave) {
+        player.save("fightCavesWaveI", wave);
+    }
+
+    public static int getFightKilnWave(Player player, int defaultWave) {
+        return Math.max(player.getI("fightKilnWaveI", 0), defaultWave);
+    }
+
+    public static void saveFightKilnWave(Player player, int wave) {
+        player.save("fightKilnWaveI", wave);
+    }
+
+    public static int lootChestMultiplier() {
+        return 10;
+    }
+
+    public static boolean wickedHood(Player player) {
+        player.getPackets().setIFText(WICKED_HOOD_INTER, 139,
+                "" + player.getDailySubI("wickedEss", 999));
+        player.getPackets().setIFText(WICKED_HOOD_INTER, 134,
+                "" + player.getDailySubI("wickedTeles", 9));
+        player.getPackets().setIFText(WICKED_HOOD_INTER, 143, "" +
+                player.getDailySubI("wickedRunes", 9));
+        return false;
+    }
+
+    public static int getWickedHoodEss(Player player) {
+        return player.getDailySubI("wickedEss", 999);
+    }
+
+    public static int getWickedHoodTeles() {
+        return 9;
+    }
+
+    public static int farmMultiplier() {
+        return 2;
+    }
+
+    public static int defenderDropRate() {
+        return 17;
+    }
+
+    public static int multiplySlayerPointsTimesFive(int amount) {
+        return amount * 5;
+    }
+
+    public static int totalXPCap() {
+        return 2_000_000_000;
+    }
+
+    public static int getLargeDungPartSizeRequirement() {
+        return 1;
+    }
+
+    public static double[] dungCombatLevelsByComplexity() {
+        return new double[]{0.20, 0.30, 0.35, 0.45, 0.55, 0.65};
+    }
+
+    public static double getDungBossMultiplier() {
+        return 0.8;
+    }
+
+    public static int dungTokenMultiplier() {
+        return 50;
+    }
+
+    public static boolean openClueReward(Player player, TreasureTrailsManager manager, Item item, int level) {
+        manager.openReward(level);
+        player.getInventory().deleteItem(item.getId(), 1);
+        return true;
+    }
+
+    public static int reduceSlayerTaskAmount(Task task) {
+        return Utils.random(Math.floorDiv(task.getMin(), 3), Math.floorDiv(task.getMax(), 3));
+    }
+
+    public static boolean tripleBarrowsReward() {
+        return true;
+    }
+
+    public static int multiplyRunespanPoints() {
+        return 10;
+    }
+
+    public static int runecraftMultiplier() {
+        return 3;
+    }
+
+    public static void saveKillCount(Player player, int[] killCount) {
+        for(int i = 0; i < killCount.length; i++)
+            player.save("godwars_killcount" + String.valueOf(i), killCount[i]);
+    }
+
+    public static int[] getKillCount(Player player) {
+        int[] killCount = new int[5];
+        for(int i = 0; i < killCount.length; i++)
+            killCount[i] = player.getI("godwars_killcount" + String.valueOf(i), 0);
+        return killCount;
+    }
+
+    public static boolean showKillCountPrompt() {
+        return false;
+    }
+
+    public static int farmingTick() {
+        return 250;
+    }
+}
