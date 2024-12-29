272dfdece1c800a8790730c8d2e8935b396f8c94
MrAshieB <MrAshieB@users.noreply.github.com>
2024-12-29 10:23:06 +0000

    Moved Penguin Respawn and Reset commands back to general Admin use.

--- src/main/java/com/rs/game/content/commands/admin/MiscTest.java
+++ src/main/java/com/rs/game/content/commands/admin/MiscTest.java
@@ -207,6 +207,42 @@ public class MiscTest {
 			}
 		});
 
+		Commands.add(Rights.ADMIN, "penguin_respawn", "Respawns both penguins and polar bear.", (p, args) -> {
+			PolarBearManager polarBearManager = PenguinServices.INSTANCE.getPolarBearManager();
+			polarBearManager.setLocation(false);
+			if (p != null) p.getPackets().sendDevConsoleMessage("Polar Bear respawned at: " + polarBearManager.getLocationName(polarBearManager.getCurrentLocationId()));
+			PenguinSpawnService penguinSpawnService = PenguinServices.INSTANCE.getPenguinSpawnService();
+			PenguinManager penguinManager = PenguinServices.INSTANCE.getPenguinHideAndSeekManager();
+			penguinSpawnService.getSpawnedNPCs().values().forEach(NPC::finish);
+			penguinSpawnService.getSpawnedNPCs().clear();
+			penguinManager.checkAndSpawn();
+			if (p != null) p.getPackets().sendDevConsoleMessage("Penguins respawned.");
+			if (p != null) Commands.processCommand(p, "penguin_status", true, false);
+		});
+
+		Commands.add(Rights.ADMIN, "penguin_reset [type]", "Resets penguins or polar bear and spawns a new set. Type can be 'penguins' or 'polarbear'.", (p, args) -> {
+			if (args.length < 1 || (!args[0].equalsIgnoreCase("penguins") && !args[0].equalsIgnoreCase("polarbear"))) {
+				p.getPackets().sendDevConsoleMessage("Usage: ::penguin_reset [penguins|polarbear]");
+				return;
+			}
+
+			String type = args[0].toLowerCase();
+
+			if (type.equals("penguins")) {
+				PenguinSpawnService penguinSpawnService = PenguinServices.INSTANCE.getPenguinSpawnService();
+				if (penguinSpawnService.removeAllSpawns()) {
+					penguinSpawnService.prepareNew();
+					World.getPlayers().forEach(player -> player.getVars().saveVarBit(5276, 0));
+				}
+				Commands.processCommand(p, "penguin_status", true, true);
+
+			} else if (type.equals("polarbear")) {
+				PolarBearManager polarBearManager = PenguinServices.INSTANCE.getPolarBearManager();
+				polarBearManager.setLocation(true);
+				p.getPackets().sendDevConsoleMessage("Polar Bear manually changed to: " + polarBearManager.getLocationName(polarBearManager.getCurrentLocationId()));
+			}
+		});
+
 		Commands.add(Rights.ADMIN, "penguin_next_reset", "Returns the date/time of the next reset.", (p, args) -> {
 			PenguinManager penguinManager = PenguinServices.INSTANCE.getPenguinHideAndSeekManager();
 
