dff1776c7d9b1ed5ce365cd227600cfb9b1bb29b
MrAshieB <MrAshieB@users.noreply.github.com>
2024-12-30 16:32:57 +0000

    Thieves Guild: Make door opening smoother after picking locks.

--- src/main/java/com/rs/game/content/skills/thieving/thievesGuild/HankyPoints.java
+++ src/main/java/com/rs/game/content/skills/thieving/thievesGuild/HankyPoints.java
@@ -9,7 +9,6 @@ import com.rs.game.model.entity.player.Skills;
 import com.rs.game.model.object.GameObject;
 import com.rs.game.tasks.WorldTasks;
 import com.rs.lib.Constants;
-import com.rs.lib.game.Animation;
 import com.rs.lib.util.Utils;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.handlers.NPCClickHandler;
@@ -239,23 +238,23 @@ public class HankyPoints {
     public static ObjectClickHandler handleNorthDoors = new ObjectClickHandler(new Object[] { 52302 }, e -> {
         if(e.getOption().equalsIgnoreCase("Open")) {
             if(e.getPlayer().getTile().getY() >= e.getObject().getY()) {
-                World.removeObjectTemporary(e.getObject(), Ticks.fromSeconds(10));
+                World.spawnObjectTemporary(new GameObject(-1, e.getObject().getType(), e.getObject().getRotation(), e.getObject().getTile()), Ticks.fromSeconds(10), true);
                 World.spawnObjectTemporary(new GameObject(e.getObjectId() + 1, e.getObject().getType(), e.getObject().getRotation() - 1, e.getObject().getTile().transform(0, -1, 0)), Ticks.fromSeconds(10), true);
                 return;
             }
             e.getPlayer().lock();
+            e.getPlayer().sendMessage("You examine the lock on the door...");
             WorldTasks.scheduleTimer(i -> {
                 switch(i) {
                     case 1 -> {
                         e.getPlayer().faceObject(e.getObject());
-                        e.getPlayer().setNextAnimation(new Animation(832));
-                        e.getPlayer().sendMessage("You examine the lock on the door...");
+                        e.getPlayer().anim(832);
                     }
                     case 3 -> {
                         if (Utils.skillSuccess(e.getPlayer().getSkills().getLevel(Skills.THIEVING), 190, 190)) {
                             e.getPlayer().sendMessage("The door swings open.");
                             e.getPlayer().getSkills().addXp(Constants.THIEVING, 210);
-                            World.removeObjectTemporary(e.getObject(), Ticks.fromMinutes(5));
+                            World.spawnObjectTemporary(new GameObject(-1, e.getObject().getType(), e.getObject().getRotation(), e.getObject().getTile()), Ticks.fromMinutes(5), true);
                             World.spawnObjectTemporary(new GameObject(e.getObjectId() + 1, e.getObject().getType(), e.getObject().getRotation() - 3, e.getObject().getTile().transform(0, -1, 0)), Ticks.fromMinutes(5), true);
                         } else {
                             e.getPlayer().sendMessage("You fail to pick the lock.");
@@ -263,7 +262,7 @@ public class HankyPoints {
                             return false;
                         }
                     }
-                    case 6 -> e.getPlayer().unlock();
+                    case 4 -> e.getPlayer().unlock();
                 }
                 return true;
             });
@@ -273,12 +272,12 @@ public class HankyPoints {
     public static ObjectClickHandler handleSouthDoors = new ObjectClickHandler(new Object[] { 52304 }, e -> {
         if(e.getOption().equalsIgnoreCase("Open")) {
             if(e.getPlayer().getY() <= e.getObject().getY()) {
-                World.removeObjectTemporary(e.getObject(), Ticks.fromSeconds(10));
+                World.spawnObjectTemporary(new GameObject(-1, e.getObject().getType(), e.getObject().getRotation(), e.getObject().getTile()), Ticks.fromSeconds(10), true);
                 World.spawnObjectTemporary(new GameObject(e.getObjectId() + 1, e.getObject().getType(), e.getObject().getRotation() + 1, e.getObject().getTile().transform(0, +1, 0)), Ticks.fromSeconds(10), true);
                 return;
             }
             if(e.getPlayer().getSkills().getLevel(Skills.THIEVING) < 35){
-                e.getPlayer().simpleDialogue("You need a thieving level of at least 35 to pick this lock.");
+                e.getPlayer().simpleDialogue("You need a Thieving level of at least 35 to pick this lock.");
                 return;
             }
             if(!e.getPlayer().getInventory().containsOneItem(1523)){
@@ -286,18 +285,18 @@ public class HankyPoints {
                 return;
             }
             e.getPlayer().lock();
+            e.getPlayer().sendMessage("You examine the lock on the door...");
             WorldTasks.scheduleTimer(i -> {
                 switch(i) {
                     case 1 -> {
                         e.getPlayer().faceObject(e.getObject());
-                        e.getPlayer().setNextAnimation(new Animation(832));
-                        e.getPlayer().sendMessage("You examine the lock on the door...");
+                        e.getPlayer().anim(832);
                     }
                     case 3 -> {
                         if (Utils.skillSuccess(e.getPlayer().getSkills().getLevel(Skills.THIEVING), 190, 190)) {
                             e.getPlayer().sendMessage("The door swings open.");
                             e.getPlayer().getSkills().addXp(Constants.THIEVING, 280);
-                            World.removeObjectTemporary(e.getObject(), Ticks.fromMinutes(5));
+                            World.spawnObjectTemporary(new GameObject(-1, e.getObject().getType(), e.getObject().getRotation(), e.getObject().getTile()), Ticks.fromMinutes(5), true);
                             World.spawnObjectTemporary(new GameObject(e.getObjectId() + 1, e.getObject().getType(), e.getObject().getRotation() + 1, e.getObject().getTile().transform(0, +1, 0)), Ticks.fromMinutes(5), true);
                         } else {
                             e.getPlayer().sendMessage("You fail to pick the lock.");
@@ -305,7 +304,7 @@ public class HankyPoints {
                             return false;
                         }
                     }
-                    case 6 -> e.getPlayer().unlock();
+                    case 4 -> e.getPlayer().unlock();
                 }
                 return true;
             });
@@ -320,14 +319,14 @@ public class HankyPoints {
             player.simpleDialogue("You need a Thieving level of at least 26 to pick this lock.");
             return;
         }
+        player.lock(1);
         player.sendMessage("You attempt to pick the lock.");
         player.anim(536);
-        player.lock(1);
         if (Utils.skillSuccess(e.getPlayer().getSkills().getLevel(Skills.THIEVING), 190, 190)) {
             player.getSkills().addXp(Constants.THIEVING, 30);
             object.setIdTemporary(e.getObjectId() + 1, Ticks.fromMinutes(1));
             player.incWeeklyI("HankyPoints", 1);
-            player.sendMessage("You find a blue hankerchief.");
+            player.sendMessage("You find a blue handkerchief.");
         }
         else {
             e.getPlayer().sendMessage("You fail to pick the lock.");
@@ -343,12 +342,12 @@ public class HankyPoints {
             player.simpleDialogue("You need a Thieving level of at least 35 to pick this lock.");
             return;
         }
+        player.lock(1);
         player.sendMessage("You attempt to pick the lock.");
         player.anim(536);
-        player.lock(1);
         if (Utils.skillSuccess(e.getPlayer().getSkills().getLevel(Skills.THIEVING), 190, 190)) {
             player.getSkills().addXp(Constants.THIEVING, 180);
-            player.sendMessage("You find a red hankerchief.");
+            player.sendMessage("You find a red handkerchief.");
             player.incWeeklyI("HankyPoints", 4);
             object.setIdTemporary(e.getObjectId() + 1, Ticks.fromMinutes(1));
 
