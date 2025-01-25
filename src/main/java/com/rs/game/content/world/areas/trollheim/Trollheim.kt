28bc914bf733688c8f0c81ce9e2a2f996ec1b511
MrAshieB <MrAshieB@users.noreply.github.com>
2025-01-25 09:11:40 +0000

    Trollheim: Climbing boots are now only required for the 3 tall rocks the player must scale during quests.
    Eadgar's Ruse quest doesn't actually remove this requirement, so reverted that.

--- src/main/java/com/rs/game/content/world/areas/trollheim/Trollheim.kt
+++ src/main/java/com/rs/game/content/world/areas/trollheim/Trollheim.kt
@@ -236,17 +236,15 @@ fun mapTrollheim() {
     onObjectClick(35391, 3748, 34877, 34889, 34878, 9306, 9305, 3803, 9304, 9303) { e ->
         if (e.player.isQuestStarted(Quest.TROLL_STRONGHOLD)) {
             when (e.objectId) {
-                3803, 9303, 9305, 9306, 34877, 34878, 34889 -> {
-                    handleObstacleInteractions(e)
-                }
-                else -> {
-                    if (e.player.equipment.getId(Equipment.FEET) == CLIMBING_BOOTS || e.player.equipment.getId(Equipment.FEET) == ROCK_CLIMBING_BOOTS || e.player.isQuestComplete(Quest.EADGARS_RUSE)) {
+                3748 -> {
+                    if (e.player.equipment.getId(Equipment.FEET) == CLIMBING_BOOTS || e.player.equipment.getId(Equipment.FEET) == ROCK_CLIMBING_BOOTS) {
                         handleObstacleInteractions(e)
                     } else {
                         e.player.sendMessage("<col=A31818>You'll need some climbing boots to go that way.</col>")
                         return@onObjectClick
                     }
                 }
+            else -> handleObstacleInteractions(e)
             }
         } else {
             e.player.sendMessage("That looks dangerous. I'll need a good reason before I venture that way.")
